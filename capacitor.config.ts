5f16b948ff05813021ebe893622f94d03eeffacf
Agustí Moll <amoll@omnium.cat>
2023-10-15 08:31:12 +0200

    fixed static directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.inergia.capitalse',
   appName: 'capitals-e',
-  webDir: 'public',
+  webDir: 'out',
   server: {
     androidScheme: 'https'
   }
